24520368e0185914b16e9fa80bb7cb8998a2cb5c
sadu.nurzhan <sadu.nurzhan@trustme.kz>
2026-02-15 13:16:44 +0500

    Initial Commit

---
+++ weatherappTests/weatherappTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherappTests.swift
+//  weatherappTests
+//
+//  Created by Nurzhan on 15.02.2026.
+//
+
+import Testing
+@testable import weatherapp
+
+struct weatherappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
